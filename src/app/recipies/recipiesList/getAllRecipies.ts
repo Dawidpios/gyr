0f45a16be874f773f299cb2974dc04d4054f9f5e
unknown <dawid.pios00@gmail.com>
2025-05-05 17:17:03 +0200

    Another fixes for prisma

--- src/app/recipies/recipiesList/getAllRecipies.ts
+++ src/app/recipies/recipiesList/getAllRecipies.ts
@@ -1,8 +1,6 @@
 
 import prisma from '@lib/prisma'
 
-
-
 export async function getAllRecipes() {
   return await prisma.recipes.findMany();
 }
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,7 +6,9 @@ const globalForPrisma = global as unknown as {
     prisma: PrismaClient
 }
 
-const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+const prisma = globalForPrisma.prisma || new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+  }).$extends(withAccelerate())
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
