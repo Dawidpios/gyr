193d2c34311b219cc51a45cef5001f31946c2be2
unknown <dawid.pios00@gmail.com>
2025-05-05 17:37:40 +0200

    Another fixes for prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,13 +2,17 @@
 import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as {
-    prisma: PrismaClient;
+    prisma?: PrismaClient;
 };
 
-const prisma = globalForPrisma.prisma || new PrismaClient({
-    log: ['query', 'info', 'warn', 'error'],
-});
+const prisma =
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['error'],
+    });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
