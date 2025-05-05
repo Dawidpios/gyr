f20b533bc19b573497f26de3ee9f94354b329e71
unknown <dawid.pios00@gmail.com>
2025-05-05 17:26:29 +0200

    Another fixes for prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,14 @@
-import { PrismaClient } from '@prisma/client'
+// filepath: d:\projekty\gyr\src\lib\prisma.ts
+import { PrismaClient } from '@prisma/client';
 
-import { withAccelerate } from '@prisma/extension-accelerate'
-
-const globalForPrisma = global as unknown as { 
-    prisma: PrismaClient
-}
+const globalForPrisma = global as unknown as {
+    prisma: PrismaClient;
+};
 
 const prisma = globalForPrisma.prisma || new PrismaClient({
     log: ['query', 'info', 'warn', 'error'],
-  }).$extends(withAccelerate())
+});
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
-export default prisma
+export default prisma;
