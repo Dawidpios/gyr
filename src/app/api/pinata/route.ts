3405637b74d93928a5cd1cc4f7bfa518017e86aa
unknown <dawid.pios00@gmail.com>
2025-05-06 17:06:38 +0200

    Trying to fix pinata auth bug

--- src/app/api/pinata/route.ts
+++ src/app/api/pinata/route.ts
@@ -4,16 +4,24 @@ import pinata from "@components/lib/pinata";
 
 export async function POST(request: NextRequest) {
   try {
-    console.log('PINATA_API_KEY=', process.env.PINATA_JWT);
-    console.log('PINATA_SECRET_API_KEY=', process.env.PINATA_GATEWAY_URL);
-    const data = await request.formData()
-    const file : File | null = data.get('file') as unknown as File
-    const { IpfsHash: cid } = await pinata.pinFileToIPFS(file)
+    const data = await request.formData();
+    const file: File | null = data.get("file") as unknown as File;
+
+    if (!file) {
+      return NextResponse.json({ error: "No file provided" }, { status: 400 });
+    }
+
+    const options = {
+      pinataMetadata: {
+        name: file.name || "uploaded_file",
+      },
+    };
+
+    const { IpfsHash: cid } = await pinata.pinFileToIPFS(file, options);
     const url = `https://gateway.pinata.cloud/ipfs/${cid}`;
-    return NextResponse.json({ url }, { status: 200 })
-  } catch(e) {
-    console.log(e)
-    return NextResponse.json({error: "Server Error"}, {status: 500})
+    return NextResponse.json({ url }, { status: 200 });
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({ error: "Server Error" }, { status: 500 });
   }
 }
-
