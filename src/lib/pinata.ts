096bfa39410a4abb605db00eadd571a9f9535b34
unknown <dawid.pios00@gmail.com>
2025-05-06 17:00:32 +0200

    Trying to fix pinata auth bug

--- src/lib/pinata.ts
+++ src/lib/pinata.ts
@@ -1,8 +1,6 @@
-import { PinataSDK } from "pinata";
-
-const pinata = new PinataSDK({
-  pinataJwt: process.env.PINATA_JWT,
-  pinataGateway: process.env.PINATA_GATEWAY_URL
-});
-
-export default pinata
+import pinataSDK from '@pinata/sdk';
+const pinata = new pinataSDK(
+  process.env.PINATA_API_KEY!,
+  process.env.PINATA_SECRET_API_KEY!
+);
+export default pinata;
